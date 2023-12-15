0cb51496780c60335164ac781581f28c0e66bb0b
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-15 07:29:20 +0100

    add /profile, logic

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Form\ChangePasswordFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
@@ -14,7 +16,9 @@ class ProfileController extends AbstractController
      * @Route("/profile", name="app_profile")
      */
     public function index(
-        Request $request
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
     ): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
@@ -22,24 +26,25 @@ class ProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // $user->setPassword(
-            //     $userPasswordHasher->hashPassword(
-            //         $user,
-            //         $form->get('plainPassword')->getData()
-            //     )
-            // );
+            $user = $this->getUser();
+            $password = $form->get('oldPassword')->getData();
+            if (!$passwordHasher->isPasswordValid($user, $password)) {
+                return $this->render('profile/index.html.twig', [
+                    'changePasswordForm' => $form->createView(),
+                    'oldPasswordError' => true,
+                ]);
+            }
 
-            // $entityManager->persist($user);
-            // $entityManager->flush();
-
-            // $this->addFlash(
-            //     'notice',
-            //     'Zarejestrowałeś użytkownika!'
-            // );
+            $newPassword = $form->get('newPassword')->getData();
+            $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
+            $entityManager->flush();
+            $this->addFlash('notice', 'Zmieniłeś hasło!');
+            return $this->redirectToRoute('app_logout');
         }
 
         return $this->render('profile/index.html.twig', [
             'changePasswordForm' => $form->createView(),
+            'oldPasswordError' => false,
         ]);
     }
 }
