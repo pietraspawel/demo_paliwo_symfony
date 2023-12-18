400b074d4224a94ab0728a84efe0c05c4fd4053d
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-18 12:13:49 +0100

    update /car/delete, change confirmation modal

--- public/assets/js/car.js
+++ public/assets/js/car.js
@@ -2,7 +2,9 @@ $().ready(function() {
     $("form.delete").on("click", "button[type='button']", (e) => {
         let button = $(e.currentTarget);
         let id = button.data("id");
+        let description = button.data("description");
         $("#deleteConfirmationModal").data("id", id);
+        $("#deleteConfirmationModal").find("span.carName").text(description);
         $("#deleteConfirmationModal").modal("show");
     });
 
