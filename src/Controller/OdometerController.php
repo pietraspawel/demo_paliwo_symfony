7f4cc20cc517c8b28532dccd8e700217ddab7a17
pietraspawel <pawel.z.pietras@gmail.com>
2023-12-20 10:28:12 +0100

    refactor OdometerController

--- src/Controller/OdometerController.php
+++ src/Controller/OdometerController.php
@@ -93,37 +93,6 @@ class OdometerController extends AbstractController
     }
 
     /**
-     * @Route("/new", name="app_odometer_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, OdometerRepository $odometerRepository): Response
-    {
-        $odometer = new Odometer();
-        $form = $this->createForm(OdometerType::class, $odometer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $odometerRepository->add($odometer, true);
-
-            return $this->redirectToRoute('app_odometer_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('odometer/new.html.twig', [
-            'odometer' => $odometer,
-            'form' => $form,
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="app_odometer_show", methods={"GET"})
-     */
-    public function show(Odometer $odometer): Response
-    {
-        return $this->render('odometer/show.html.twig', [
-            'odometer' => $odometer,
-        ]);
-    }
-
-    /**
      * @Route("/{id}/edit", name="app_odometer_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, Odometer $odometer, OdometerRepository $odometerRepository): Response
